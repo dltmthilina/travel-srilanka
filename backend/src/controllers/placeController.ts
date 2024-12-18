45d059326e48856177a4b6cde62efc8f72c5f56f
dltmthilina <dltmthilinamaduranga@gmail.com>
2024-12-18 16:47:52 +0530

    add placeController with createPlace method

--- backend/src/controllers/placeController.ts
+++ backend/src/controllers/placeController.ts
@@ -1,7 +1,20 @@
-const createPlace = () => {};
-const getAllPlacesByUid = () => {};
-const getPlaceByUid = () => {};
-const getPlaceByPid = () => {};
-const getPlacesByDid = () => {};
-const getPlacesByCategory = () => {};
+import { Request, Response, NextFunction } from "express";
+import { TourPlace } from "../models/place";
 
+exports.createPlace = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { title, description, location, userId } = req.body;
+  const newPlace = new TourPlace(null, title, description, location, userId);
+  newPlace
+    .createPlace()
+    .then(() => {})
+    .catch(() => {});
+};
+exports.getAllPlacesByUid = () => {};
+exports.getPlaceByUid = () => {};
+exports.getPlaceByPid = () => {};
+exports.getPlacesByDid = () => {};
+exports.getPlacesByCategory = () => {};
