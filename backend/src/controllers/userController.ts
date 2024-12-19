10708c053ac25f24b67fa33c1f94ddf6a6342990
dltmthilina <dltmthilinamaduranga@gmail.com>
2024-12-19 23:26:11 +0530

    add method to fetch user by user id into userController

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -1,7 +1,20 @@
+import { Request, Response, NextFunction } from "express";
+
 const signUp = () => {};
 const signIn = () => {};
+const getUserByUserId = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const userId = parseInt(req.params.id, 10);
+
+  try {
+  } catch (error) {}
+};
 
 export default {
   signUp,
   signIn,
+  getUserByUserId,
 };
