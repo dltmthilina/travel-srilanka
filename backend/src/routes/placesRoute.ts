f902463c0e854ab1ece3885a9bce4b4479d24213
dltmthilina <dltmthilinamaduranga@gmail.com>
2024-12-19 17:19:21 +0530

    add validation for required details of place

--- backend/src/routes/placesRoute.ts
+++ backend/src/routes/placesRoute.ts
@@ -1,9 +1,18 @@
 import express from "express";
+import { check } from "express-validator";
 import placeControllers from "../controllers/placeController";
 
 const router = express.Router();
 
-router.post("/create-place", placeControllers.createPlace);
+router.post(
+  "/create-place",
+  [
+    check("title").not().isEmpty(),
+    check("description").not().isEmpty(),
+    check("location").not().isEmpty(),
+  ],
+  placeControllers.createPlace
+);
 router.get("/", placeControllers.getAllPlacesByUid);
 router.get("/:pid", placeControllers.getPlaceByPid);
 router.put("/:pid", placeControllers.updatePlace);
