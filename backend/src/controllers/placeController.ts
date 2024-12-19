f0396d534bf6235bba81f9b26738a2b85ddee187
dltmthilina <dltmthilinamaduranga@gmail.com>
2024-12-19 17:23:22 +0530

    implement featurs of create place contoller to pass relavant unit tests

--- backend/src/controllers/placeController.ts
+++ backend/src/controllers/placeController.ts
@@ -1,19 +1,26 @@
 import { Request, Response, NextFunction } from "express";
+import { validationResult } from "express-validator";
 import TourPlace from "../models/place";
+import HttpError from "../models/httpError";
 
 const createPlace = async (req: Request, res: Response, next: NextFunction) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    next(new HttpError("Invalid inputs, please check your data", 400));
+  }
   try {
     const { title, description, location, userId } = req.body;
-    if (!title || !description || !location || !userId) {
-       res.status(400).json({ message: "Missing required fields" });
-    }
     const newPlace = new TourPlace(null, title, description, location, userId);
-  } catch (error) {}
+    const result = await newPlace.createPlace();
+    res.status(201).json({
+      message: "Place created successfully!",
+      placeId: result.insertId,
+    });
+  } catch (error) {
+    next(new HttpError("Failed to create place. Please try again later", 500));
+  }
 };
-const getAllPlacesByUid = () => {
-  try {
-  } catch (error) {}
-};
+const getAllPlacesByUid = () => {};
 const getPlaceByPid = () => {};
 const getPlacesByDid = () => {};
 const getPlacesByCategory = () => {};
