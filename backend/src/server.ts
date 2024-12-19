4cf6548169ac68958eb371c0662307a272a21666
dltmthilina <dltmthilinamaduranga@gmail.com>
2024-12-19 10:52:37 +0530

    add custom error handling middleware

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,11 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import bodyParser from "body-parser";
 const placeRoutes = require("./routes/placesRoute");
 const userRoutes = require("./routes/userRoutes");
+
+interface CustomError extends Error {
+  code?: number; // Optional property for error code
+}
 
 const app = express();
 const PORT = 3000;
@@ -12,12 +16,22 @@ app.use(bodyParser.json());
 app.use("/users", userRoutes);
 app.use("/places", placeRoutes);
 
+app.use(
+  (error: CustomError, req: Request, res: Response, next: NextFunction) => {
+    if (res.headersSent) {
+      return next(error);
+    }
+    res.status(error.code || 500);
+    res.json({ message: error.message || "An unknown error occured!" });
+  }
+);
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Server is running!" });
+});
+
 const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-module.exports = server;
-
-/* export function add(a: number, b: number): number {
-  return a + b;
-} */
+export default server;
