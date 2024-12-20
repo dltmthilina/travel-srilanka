d8d969ad1c3540e599d5f4327c40567cbd508f42
dltmthilina <dltmthilinamaduranga@gmail.com>
2024-12-20 11:47:13 +0530

    Implement getUserById method to pass unit tests

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -1,4 +1,6 @@
 import { Request, Response, NextFunction } from "express";
+import HttpError from "../models/httpError";
+import Tourist from "../models/user";
 
 const signUp = () => {};
 const signIn = () => {};
@@ -7,10 +9,25 @@ const getUserByUserId = async (
   res: Response,
   next: NextFunction
 ) => {
-  const userId = parseInt(req.params.id, 10);
-
+  const userId = parseInt(req.params.uid, 10);
+  if (userId === null || userId === undefined) {
+    next(new HttpError("Invalid user ID", 400));
+    return;
+  }
+  if (isNaN(userId)) {
+    next(new HttpError("Invalid user ID", 400));
+    return;
+  }
   try {
-  } catch (error) {}
+    const tourist = await Tourist.getById(userId);
+    if (!tourist) {
+      next(new HttpError("User not found", 404));
+      return;
+    }
+    res.status(200).json(tourist);
+  } catch (error) {
+    next(new HttpError("Invalid user ID", 400));
+  }
 };
 
 export default {
--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -6,7 +6,6 @@ interface UserProps {
   fName?: string;
   lName?: string;
   email: string;
-  password: string;
 }
 
 export default class Tourist implements UserProps {
@@ -15,7 +14,6 @@ export default class Tourist implements UserProps {
   lName?: string;
   email: string;
   country?: string;
-  password: string;
   dpImageUrl?: string;
 
   constructor(
@@ -24,15 +22,13 @@ export default class Tourist implements UserProps {
     lname: string,
     dpImageUrl: string,
     email: string,
-    country: string,
-    password: string
+    country: string
   ) {
     this.id = id;
     this.fName = fname;
     this.lName = lname;
     this.email = email;
     this.country = country;
-    this.password = password;
     this.dpImageUrl = dpImageUrl;
   }
 
@@ -43,15 +39,25 @@ export default class Tourist implements UserProps {
       this.lName,
       this.email,
       this.country,
-      this.password,
       this.dpImageUrl,
     ]);
     return result;
   }
-  async getById(userId: number) {
+  static async getById(userId: number): Promise<Tourist | null> {
     const query = `SELECT * FROM users WHERE id =?`;
-    const [rows] = await db.execute(query, [userId]);
-    return rows;
+    const [rows]: any[] = await db.execute(query, [userId]);
+    if (rows.length === 0) {
+      return null; // No user found
+    }
+    const row = rows[0];
+    return new Tourist(
+      row.id,
+      row.firstName,
+      row.lastName,
+      row.dpImage,
+      row.email,
+      row.country
+    );
   }
   getByPlaceId() {}
   getAll() {}
