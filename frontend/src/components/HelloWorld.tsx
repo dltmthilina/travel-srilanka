4a80c2dd5fad9dbf6a14a736c591e5ddca6c0e46
dltmthilina <dltmthilinamaduranga@gmail.com>
2025-02-01 01:03:54 +0530

    pipeline test-1

--- frontend/src/components/HelloWorld.tsx
+++ frontend/src/components/HelloWorld.tsx
@@ -2,7 +2,7 @@ const HelloWorld = () => {
     return (
         <>
             <h1>Hello World</h1>
-            <h3> frontend Pipeline test</h3>
+            <h3> frontend Pipeline ubuntu EC2- test 1</h3>
             <hr />
             <h3>Environmental variables:</h3>
             <p>
