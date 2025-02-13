cc2fe7b9a60d481b9b4be00c93ef6bafe64684be
dltmthilina <dltmthilinamaduranga@gmail.com>
2025-02-13 23:18:07 +0530

    change jest.config

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -2,7 +2,8 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
+  testMatch: ["**/src/test/**/*.test.ts"],
   transform: {
-    "^.+.tsx?$": ["ts-jest",{}],
+    "^.+.tsx?$": ["ts-jest", {}],
   },
 };
